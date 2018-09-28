679b025cb403abbe531d8234f00d53b5ed40897e
Emric <emric.mansson@gmail.com>
2018-09-28 23:56:30 +0200

    Document keyboard input

--- src/keyboard_input.js
+++ src/keyboard_input.js
@@ -1,10 +1,24 @@
+/**
+ * Used for recording and subscribing on keyboard input.
+ */
 class KeyboardInput {
+  /**
+   * Add a function to be called whenever a specific button is pressed.
+   * @param {string}   - The keycode to subscribe to.
+   * @param {function} - The callback to be run each time the provided key is
+   *                     pressed.
+   */
   static on(keycode, callback) {
     this.actions = KeyboardInput.actions || {};
     this.actions[keycode] = this.actions[keycode] || [];
     this.actions[keycode].push(callback);
   }
 
+  /**
+   * Subscribe on the keydown events.
+   * Calls the provided functions when an appropriate key is pressed.
+   * @private
+   */
   static subscribe() {
     document.addEventListener('keydown', (ev) => {
       if (this.actions[ev.key]) {
